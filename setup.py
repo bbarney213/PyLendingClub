1dd55566c97da8f5033d7be746459b7718a88b50
Brandon <brandon.barney213@yahoo.com>
2018-06-06 22:24:55 -0400

    Adding pip setup.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'pylendingclub',
+    packages = ['pylendingclub'],
+    version = 'v1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A Python wrapper for the Lending Club API.',
+    author = 'Brandon Barney',
+    author_email = 'brandon.barney213@yahoo.com',
+    url = 'https://github.com/bbarney213/PyLendingClub-Wrapper',
+    download_url = 'https://github.com/bbarney213/PyLendingClub-Wrapper/archive/v1.0.tar.gz',
+    keywords = ['python', 'lendingclub'],
+    classifiers = [],
+)
