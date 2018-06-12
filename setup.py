72f4000ba2a7f509f52fa194e896431894baed74
bbarney213 <28009295+bbarney213@users.noreply.github.com>
2018-06-12 13:29:21 -0400

    Updated setup file.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'pylendingclub',
     packages = ['pylendingclub'],
-    version = 'v1.7',  # Ideally should be same as your GitHub release tag version
+    version = 'v2.0.1',  # Ideally should be same as your GitHub release tag version
     description = 'A Python wrapper for the Lending Club API.',
     author = 'Brandon Barney',
     author_email = 'brandon.barney213@yahoo.com',
     url = 'https://github.com/bbarney213/PyLendingClub-Wrapper',
-    download_url = 'https://github.com/bbarney213/PyLendingClub-Wrapper/archive/v1.7.tar.gz',
+    download_url = 'https://github.com/bbarney213/PyLendingClub-Wrapper/archive/v2.0.1.tar.gz',
     keywords = ['python', 'lendingclub'],
     classifiers = [],
 )
