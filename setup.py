d2da9d795ba97d55b76c0016cb0a2bfc68c8d223
bbarney213 <28009295+bbarney213@users.noreply.github.com>
2018-10-16 09:41:43 -0400

    Updating setup script.

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(name='pylendingclub',
-      package_dir = {'' : 'src'},
-      packages = ['pylendingclub'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')]
       description = 'A Python wrapper for automation of the Lending Club API.',
       author = 'Brandon Barney',
       author_email = 'brandon.barney213@yahoo.com',
       keywords = ['python', 'lendingclub'],
       url = 'https://github.com/bbarney213/PyLendingClub',
-      classifiers = [],
+      classifiers = [
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',],
       setup_requires=['pytest-runner'],
       tests_require=['pytest']
 
