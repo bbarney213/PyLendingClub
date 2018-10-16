c53317a75d13b31200088c392fdaba3fe60421dc
bbarney213 <28009295+bbarney213@users.noreply.github.com>
2018-10-16 08:13:36 -0400

    Added init files

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(name='pylendingclub',
-      package_dir = {'' : 'src'}
+      package_dir = {'' : 'src'},
       packages = ['pylendingclub'],
       description = 'A Python wrapper for automation of the Lending Club API.',
       author = 'Brandon Barney',
