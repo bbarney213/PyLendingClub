dad69ccbed417ac7bf6c1b29935fa902709ab70f
bbarney213 <28009295+bbarney213@users.noreply.github.com>
2018-11-06 09:07:02 -0500

    Added support for Sandbox account.

--- src/pylendingclub/config.py
+++ src/pylendingclub/config.py
@@ -1,4 +1,8 @@
 LC_API_VERSION = 'v1'
+LC_API_URL = 'https://api.lendingclub.com/api/investor/{}/'.format(LC_API_VERSION)
+
+LC_SANDBOX_API_VERSION = 'v1'
+LC_SANDBOX_API_URL = 'https://api-sandbox.lendingclub.com/api/investor/{}/'.format(LC_SANDBOX_API_VERSION)
 
 LC_MIN_NOTE_INVESTMENT = 25
 LC_INVESTMENT_DENOMINATION = 25
