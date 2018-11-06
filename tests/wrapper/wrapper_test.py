dbca0407930b6a3b8669e037d817276b2d4529ac
bbarney213 <28009295+bbarney213@users.noreply.github.com>
2018-11-06 10:53:45 -0500

    Update tests/wrapper/wrapper_test.py

--- tests/wrapper/wrapper_test.py
+++ tests/wrapper/wrapper_test.py
@@ -1,18 +1,17 @@
+from pylendingclub.wrapper.session import LendingClubSession
+import requests
 if __name__ == '__main__':
     import sys
     from os.path import dirname, abspath, join
     package_path = join(dirname(dirname(dirname(abspath(__file__)))), 'src')
     sys.path.insert(0, package_path)
 
-import requests
-
-from pylendingclub.wrapper.session import LendingClubSession
 
 ACCOUNT_SUMMARY_KEYS = ['investorId', 'availableCash', 'accountTotal',
-    'accruedInterest', 'infundingBalance', 'receivedInterest',
-    'receivedPrincipal', 'receivedLateFees', 'outstandingPrincipal',
-    'totalNotes', 'totalPortfolios', 'netAnnualizedReturn',
-    'adjustments']
+                        'accruedInterest', 'infundingBalance', 'receivedInterest',
+                        'receivedPrincipal', 'receivedLateFees', 'outstandingPrincipal',
+                        'totalNotes', 'totalPortfolios', 'netAnnualizedReturn',
+                        'adjustments']
 AVAILABLE_CASH_KEYS = ['investorId', 'availableCash']
 NOTES_KEYS = DETAILED_NOTES_KEYS = ['myNotes']
 PORTFOLIOS_OWNED_KEYS = ['myPortfolios']
@@ -20,14 +19,17 @@ FILTERS_KEYS = []
 FUNDS_PENDING_KEYS = []
 LISTED_LOANS_KEYS = ['asOfDate', 'loans']
 
+
 def check_response(response, keys, name):
     try:
-        assert isinstance(response, requests.Response), 'Response provided is not a Response object.'
+        assert isinstance(
+            response, requests.Response), 'Response provided is not a Response object.'
         assert response.status_code == 200, 'Response from the API was not successful. Status Code {}: {}'.format(
             response.status_code, response.reason)
         assert isinstance(response.json(), dict), 'Unable to get JSON from response.'
         if len(response.json()) > 0:
-            assert set(keys).issubset(response.json().keys()), 'Response JSON missing expected fields.'
+            assert set(keys).issubset(response.json().keys()
+                                      ), 'Response JSON missing expected fields.'
         else:
             print('Unable to check the response keys for the {} resource. Response is empty.'.format(name))
     except AssertionError as e:
@@ -40,6 +42,7 @@ def check_response(response, keys, name):
             pass
         raise
 
+
 def test_wrapper():
     session = LendingClubSession.from_environment_variables(True)
     check_response(session.account.summary, ACCOUNT_SUMMARY_KEYS, 'Account Summary')
@@ -51,6 +54,7 @@ def test_wrapper():
     check_response(session.account.funds.pending, FUNDS_PENDING_KEYS, 'Funds Pending')
     check_response(session.loan.listed_loans(), LISTED_LOANS_KEYS, 'Listed Loans')
     print('All tests passed.')
+
 
 """
 Tests Missing:
